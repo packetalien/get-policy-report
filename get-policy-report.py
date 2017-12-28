24ec7bbdd632d29e95ac685accda9dbed8f26fef
packetalien <rwporter@pedantictheory.com>
2017-12-27 19:04:06 -0700

    Add files via upload
    Initial Upload.

---
+++ get-policy-report.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# ========================================================================
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+# ========================================================================
+# Requests library is not standard and
+# may require extra install.
+# Run the following at a command prompt (linux/macOS)
+# ========================================================================
+# pip install requests
+# ========================================================================
+# TODO
+# - Add string output formatting
+
+# This code is based on interactions with Panorama.
+
+
+try:
+    import requests
+    from requests.packages.urllib3.exceptions import InsecureRequestWarning
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+except ImportError:
+    raise ValueError('requests support not available, please install module')
+import xml.etree.ElementTree as ET
+import re
+
+
+# Default for management ports on Palo Alto Networks devices.
+
+
+fwip = '192.168.0.1'
+username = 'admin'
+password = 'admin'
+
+# Initializing default generate key url
+
+keycall = "https://%s/api/?type=keygen&user=%s&password=%s" % (fwip,username,password)
+
+
+def getfwipfqdn():
+    while True:
+        try:
+            fwipraw = raw_input("Please enter an IP or FQDN: ")
+            ipr = re.match(r"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", fwipraw)
+            fqdnr = re.match(r"(?=^.{4,253}$)(^((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}$)", fwipraw)
+            print("You entered %s\n" % fwipraw)
+            if ipr:
+                print("True")
+                break
+            elif fqdnr:
+                print("True")
+                break
+            else:
+                print("There was something wrong with your entry. Please try again.\n")
+        except:
+            print("There was some kind of problem entering your IP or FQDN. Please try again.\n")
+    return fwipraw
+
+
+def getuname():
+    while True:
+        try:
+            username = raw_input("Please enter a user name (note, must have API access): ")
+            usernamer = re.match(r"^[a-z0-9_-]{3,24}$", username) # 3 - 24 characters {3,24}
+            print("You entered %s" % username)
+            if usernamer:
+                print("True")
+                break
+            else:
+                print("There was something wrong with your entry. Please try again.\n")
+            print("You entered %s\n" % username)
+        except:
+            print("There was some kind of problem entering your user name. Please try again.\n")
+    return username
+
+def getpass():
+    while True:
+        try:
+            password = raw_input("Please enter your password: ")
+            passwordr = re.match(r"^.{5,50}$",password) # simple validate PANOS has no password characterset restrictions
+            print("You entered %s\n" % password)
+            if passwordr:
+                print("True")
+                break
+            else:
+                print("There was something wrong with your entry. Please try again.\n")
+        except:
+            print("There was some kind of problem entering your password. Please try again.\n")
+    return password
+
+
+def getkey(fwip):
+    try:
+        fwipgetkey = fwip
+        username = getuname()
+        password = getpass()
+        keycall = "https://%s/api/?type=keygen&user=%s&password=%s" % (fwipgetkey,username,password)
+        r = requests.get(keycall, verify=False)
+        tree = ET.fromstring(r.text)
+        if tree.get('status') == "success":
+            apikey = tree[0][0].text
+    except requests.exceptions.ConnectionError as e:
+        print("There was a problem connecting to the firewall.  Please check the connection information and try again.")
+    return apikey
+
+def getpolicy(passkey,fwip):
+    try:
+        type = "config"
+        action = "get"
+        devicegroup = "Shared"
+        fwkey = passkey
+        fwipl = fwip
+        xpath = "/config/shared/pre-rulebase/security"
+        call = "https://%s/api/?type=%s&action=%s&xpath=%s&key=%s" % (fwipl, type, action, xpath, fwkey)
+        r = requests.get(call, verify=False)
+        #tree = ET.fromstring(r.text)
+        return r.text
+    except requests.exceptions.ConnectionError as e:
+        print("There was a problem in getting your policies. \n Please vent frustrations in a safe manner and throw a candy bar at the wall! \n If this is helpful the error was captured as: " + e)
+
+def main():
+    try:
+        fwip = getfwipfqdn()
+        mainkey = getkey(fwip)
+        results = getpolicy(mainkey,fwip)
+        print(results)
+        print(r.text)
+    except:
+        print("Something happened and your output didn't.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,24 @@
+Description: 
+
+This is a script that gets the pre-rule base from Panorama. It is still alpha/development code. 
+
+Usage:
+
+$ python get-policy-report.py
+
+To Do:
+
+# TODO #
+- Add string output formatting
+- Add Date Time Stamps
+- Add File Output Options
+- Add Error Checking
+- Add Logging
+
+
+
+
+
+Author: 
+
+	Richard Porter packetalien@packetalien.com
